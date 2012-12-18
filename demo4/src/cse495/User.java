f6f4d5def03e08ae7bfc52facfcdeddcf872da3f
Hakan Dilek <hakandilek@gmail.com>
2012-12-18 14:22:57 +0100

    .hashCode() call

--- demo4/src/cse495/User.java
+++ demo4/src/cse495/User.java
@@ -18,8 +18,11 @@ public class User {
 	public static void main(String[] args) {
 		User user1 = new User("Some", "User");
 		System.out.println(user1);
+		System.out.println("user1.hashCode() : " + user1.hashCode());
 		User user2 = new User("Another", "User");
+		System.out.println("user2.hashCode() : " + user2.hashCode());
 		System.out.println(user2);
+		System.out.println("user2.hashCode() : " + user2.hashCode());
 		System.out.format("%s == %s : %b\n", user1, user2, user1.equals(user2));
 		user2.setFirstName("Some");
 		System.out.format("%s == %s : %b\n", user1, user2, user1.equals(user2));
